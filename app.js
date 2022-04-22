2517ef475e861e84cbe8c72e914f46b555c2c5c0
Sheepshenko <sheepshenko@gmail.com>
2022-04-21 20:44:27 -0500

    Retorno de objeto /node

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.get('/launchx', (req, res) => {
     res.send("Bienvenidos a launchX")
 })
 
+app.get('/node', (req, res) =>{
+    const explorer1 = {id:1, name: "Explorer"}
+    const explorer2 = {id:2, name: "Explorer"}
+    const explorer3 = {id:3, name: "Explorer"}
+    res.send([explorer1,explorer2,explorer3])
+})
+
+
+
 app.listen(port, () => {
     console.log("Server Listo!")
 })
